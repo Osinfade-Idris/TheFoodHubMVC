539632a3b65a7d8ce0f4ddd4aec6720ccf6a76d8
Idris.Osinfade <osinfadeidris@gmail.com>
2022-11-28 14:32:08 +0100

    Dashboard Details

--- TheFoodHubMVC/Controllers/DashboardController.cs
+++ TheFoodHubMVC/Controllers/DashboardController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheFoodHubMVC.Data;
+using TheFoodHubMVC.Models;
 
 namespace TheFoodHubMVC.Controllers
 {
     public class DashboardController : Controller
     {
-        [Authorize]
-
-        public IActionResult Index()
+        
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+        public DashboardController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var staffcount = await _userManager.Users.CountAsync();
+            TempData["staffcount"] = staffcount - 1;
+
+            var categorycount = await _context.ProductCategories.CountAsync();
+            TempData["categorycount"] = categorycount;
+
+            var allproduct = await _context.AllProducts.CountAsync();
+            TempData["allproduct"] = allproduct;
+
             return View();
         }
     }
---
+++ TheFoodHubMVC/Controllers/DashboardData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheFoodHubMVC.Models;
+
+namespace TheFoodHubMVC.Controllers
+{
+    public class DashboardData : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DashboardData(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var staffcount = await _userManager.Users.CountAsync();
+            TempData["staffcount"] = staffcount;
+            return View();
+        }
+    }
+}
